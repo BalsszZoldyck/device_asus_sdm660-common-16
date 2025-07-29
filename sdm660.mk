06563c3f956a85041f8180c293ee9fc9cc89df82
texascake <tiktodz@soulvibe-team.cloud>
2025-07-29 18:39:04 +0000

    sdm660-common: Update USB debugging props
    because was making it in final deafult prop to persist.sys.usb.config=mtp,adb,adb
    
    Change-Id: Ifb5210c51689e53417de8e95b579343aca3f9a1b

--- sdm660.mk
+++ sdm660.mk
@@ -522,6 +522,12 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.basic \
     android.hardware.usb.gadget-service.qti
 
+# USB debugging
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp \
+    ro.adb.secure=0 \
+    ro.debuggable=1
+
 # Vibrator
 $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
