a232182e21fbd9632834f40fd34ffb0b180c92d5
texascake <tiktodz@soulvibe-team.cloud>
2025-07-29 18:04:33 +0000

    sdm660-common: Import and switch to local usb_compositions.conf
    Change-Id: Ie1d6bbca3aa6e10aafcee0c37de370d3da65aa13
    Signed-off-by: texascake <tiktodz@soulvibe-team.cloud>

--- sdm660.mk
+++ sdm660.mk
@@ -518,7 +518,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    usb_compositions.conf \
+    X00TD_usb_compositions.conf \
     android.hardware.usb@1.3-service.basic \
     android.hardware.usb.gadget-service.qti
 
